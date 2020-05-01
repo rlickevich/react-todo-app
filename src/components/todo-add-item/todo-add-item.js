a694e604a64a75b8a43b7fb63abaea6203e1899b
Roman Lickevich <r.lickevich@gmail.com>
2020-05-01 22:41:32 +0200

    add submit on form

--- src/components/todo-add-item/todo-add-item.js
+++ src/components/todo-add-item/todo-add-item.js
@@ -3,21 +3,39 @@ import React, { Component } from 'react';
 import './todo-add-item.css';
 
 export default class TodoAddItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      label: '',
+    };
+
+    this.onLableChange = (e) => {
+      this.setState({
+        label: e.target.value,
+      });
+    };
+
+    this.onSubmit = (e) => {
+      e.preventDefault();
+      this.props.onItemAdded(this.state.label);
+      this.setState({
+        label: '',
+      });
+    };
+  }
+
   render() {
     return (
-      <form className="todo-add-item">
+      <form className="todo-add-item d-flex" onSubmit={this.onSubmit}>
         <input
           type="text"
           className="form-control"
           onChange={this.onLableChange}
           placeholder="What needs to be done"
+          value={this.state.label}
         />
-        <button
-          className="btn btn-outline-secondary float-left"
-          onClick={() => this.props.onItemAdded('Hello world')}
-        >
-          Add Item
-        </button>
+        <button className="btn btn-outline-secondary">Add Item</button>
       </form>
     );
   }
