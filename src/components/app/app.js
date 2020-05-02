9ec77f33bbece9bfd23338074ca4634400047e36
Roman Lickevich <r.lickevich@gmail.com>
2020-05-02 19:59:10 +0200

    add search todo

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -29,6 +29,7 @@ export default class App extends Component {
         this.createTodoItem('Make Awesome App'),
         this.createTodoItem('Have a lunch'),
       ],
+      term: '',
     };
 
     this.deleteItem = (id) => {
@@ -80,22 +81,36 @@ export default class App extends Component {
         };
       });
     };
+
+    this.onSearchChange = (term) => {
+      this.setState({ term });
+    };
+
+    this.searchTodos = (items, term) => {
+      if (term.length === 0) {
+        return items;
+      }
+      return items.filter((item) =>
+        item.label.toLowerCase().includes(term.toLowerCase()),
+      );
+    };
   }
 
   render() {
-    const { todoData } = this.state;
+    const { todoData, term } = this.state;
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
+    const searchedTodos = this.searchTodos(todoData, term);
 
     return (
       <div className="todo-app">
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className="top-panel d-flex">
-          <SearchPanel />
+          <SearchPanel onSearchChange={this.onSearchChange} />
           <ItemStatusFilter />
         </div>
         <TodoList
-          todos={todoData}
+          todos={searchedTodos}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
